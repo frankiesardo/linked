82a7d15a90d8910d4360066093ec4a7188f3e6f5
frankiesardo <fran.sardo@gmail.com>
2014-04-16 11:07:24 +0200

    Promote first version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject linked "0.1.0-SNAPSHOT"
+(defproject com.github.frankiesardo/linked "0.1.0"
   :description "Efficient ordered map and set"
   :url "http://github.com/frankiesardo/linked"
   :license {:name "Eclipse Public License"
