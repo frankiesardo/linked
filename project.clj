f9c96d5e003975271ec9a4b00a4e6c960ce6a6d8
frankiesardo <fran.sardo@gmail.com>
2014-10-26 13:12:03 +0000

    Cut new release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject frankiesardo/linked "1.0.4"
+(defproject frankiesardo/linked "1.0.5"
   :description "Efficient ordered map and set"
   :url "http://github.com/frankiesardo/linked"
   :license {:name "Eclipse Public License"
