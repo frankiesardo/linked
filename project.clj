86745fd5960fcbf7fd79be663e1891236f378873
Frankie Sardo <fran.sardo@gmail.com>
2015-03-14 00:14:35 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -26,7 +26,6 @@
             "check" ["do" "clean," "cljx" "once," "check"]
             "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]}
   :deploy-repositories {"clojars" {:url "https://clojars.org/repo"
-                                   :signing {:gpg-key "fran.sardo@gmail.com"}
                                    :username [:gpg :env/clojars_username]
                                    :password [:gpg :env/clojars_password]}}
 
