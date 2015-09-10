a0e9cef202b51f64079b533acc2f0400ea4ab92c
Francesco Sardo <fran.sardo@gmail.com>
2015-09-10 20:26:29 +0100

    Version 1.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject frankiesardo/linked "1.2.3-SNAPSHOT"
+(defproject frankiesardo/linked "1.2.3"
   :description "Efficient ordered map and set"
   :url "http://github.com/frankiesardo/linked"
   :license {:name "Eclipse Public License"
